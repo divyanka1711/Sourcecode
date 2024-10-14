7b57a4bc5d349604a37ec1d376ec8a0e26b4825d
divyanka1711 <2k22.cscore.2212314@gmail.com>
2024-10-15 00:09:25 +0530

    Create SimpleSum.java

---
+++ SimpleSum.java
@@ -0,0 +1,9 @@
+class SimpleSum {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c=a+b;
+        System.out.println("sum is "+ c);
+        
+    }
+}
